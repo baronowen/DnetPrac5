99b8f6d62daf8df53b154cffd71121727fe45479
baronowen <owenashby@hotmail.nl>
2018-05-31 15:12:34 +0200

    Added product service

--- ShopServerLibrary/Product.cs
+++ ShopServerLibrary/Product.cs
@@ -14,8 +14,38 @@ namespace ShopServerLibrary
 
         public double Price { get; set; }
 
-        public int amount { get; set; }
+        public int Amount { get; set; }
+
+        public List<Product> Products { get; set; }
 
 
+        public string GenerateProductList()
+        {
+            this.Products.Add(
+                new Product
+                {
+                    Name = "Carrot",
+                    Amount = 20,
+                    Price = 2.50,
+                    Id = 1
+                });
+            this.Products.Add(
+                new Product
+                {
+                    Name = "Apple",
+                    Amount = 30,
+                    Price = 1,
+                    Id = 2
+                });
+            this.Products.Add(
+                new Product
+                {
+                    Name = "Lettuce",
+                    Amount = 50,
+                    Price = 3,
+                    Id = 3
+                });
+            return this.Products.ToString();
+        }
     }
 }
